e59c08492e301b201355326436e21243d5025539
Jonathan Hernandez M <jonathan.a.hernandez.m@gmail.com>
2023-04-20 18:42:36 -0400

    revision 2

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,78 @@
 // define variablese a utilizar en la App.
-
 const registros =[]
-const hist_registros=[]
+let hist_registros=[]
 let regId = 0
 let editandoRegistro = false
 let editandoIndice = 0
-//readDataLS()
+let modoDemo = false
+let cargaLS = false
+let value = 0
+readDataLS() // valida si existen o no datos en LS.
+updProjectStatus() //carga algunos extras del proyecto.
 
+function demo(){
+    if (modoDemo == false){
+    modoDemo = true
+    console.log('Clic en el botón Demo, carga los registros de ejemplo.')
 // define 3 tareas de ejemplo para desplegar en la aplicación
-// const registro = {
-//     id: 17388935,
-//     name: 'Jonathan',
-//     lastName: 'Hernandez',
-//     phone: '978778829',
-//     desc: 'Limpiar algo rápido',
-//     status: 'En proceso',
-//     assignDate: '2023-04-10'
-// }
-// registros.push(registro)
-// console.log(registros)
+    const d1 = {
+        regId:Date.now()*(Math.round(Math.random()*100)),
+        id: '17388935',
+        name: 'Daniela',
+        lastname: 'Navarro',
+        projectStatus: 'Completado',
+        desc: 'Desarrollar UI para CRUD',
+        assignDate: '2023-03-29'}
+    const d2 = {
+        regId:Date.now()*(Math.round(Math.random()*100)),
+        id: '17388935',
+        name: 'Jonathan',
+        lastname: 'Hernandez',
+        projectStatus: 'Asignado',
+        desc: 'Terminar código JavaScript de CRUD',
+        assignDate: '2023-04-10',
+    }
+    const d3 = {
+        regId:Date.now()*(Math.round(Math.random()*100)),
+        id: '7831008',
+        name: 'José',
+        lastname: 'Hernández',
+        projectStatus: 'Pendiente',
+        desc: 'Habilitar modo Demo en CRUD',
+        assignDate: '2023-04-15'
+    }
+    registros.push(d1)
+    registros.push(d2)
+    registros.push(d3)
+    console.log(registros)
+    saveDataLS()
+    readDataLS()
+    updProjectStatus()
+    }else{
+        console.log('Demo ya fue ejecutado')
+    }
+}
+
+const demoButton =document.getElementById('demo')
+demoButton.addEventListener('click',demo)
 
 const addButton = document.getElementById('nuevoRegistro')
 addButton.addEventListener('click',creaRegistro)
 
 function creaRegistro(event){
-    console.log(editandoRegistro)
     event.preventDefault()
-    if (editandoRegistro === false){
-    regId = registros.length // Aumenta en 1 el número del ID de Registro
-    const registro = readform()
-    registros.push(registro);
-    insertRecord(registro,regId)
-    saveDataLS()
-    // readDataLS()
-    updProjectStatus()
-    limpiarFormulario()
+    if (editandoRegistro == false){
+        const registro = readform()
+        registros.push(registro);
+        cargaLS = false
+        insertRecord(registro)
+        updProjectStatus()
+        limpiarFormulario()
+        saveDataLS()
     }else{
-    console.log(editandoIndice)
-    saveRecordupdate(editandoIndice)
-    updProjectStatus()
+        console.log(editandoIndice)
+        saveRecordupdate(editandoIndice)
+        updProjectStatus()
     }
 }
 
@@ -55,7 +89,7 @@ function readform(){
     const assignDateInput = document.getElementById('assignDate')
     const descInput = document.getElementById('desc')
     const registro = {
-        regId: regId,
+        regId:regId,
         id: idInput.value,
         name: nameInput.value,
         lastname: lastNameInput.value,
@@ -66,8 +100,12 @@ function readform(){
     return registro    
 }
 
-function insertRecord(registro, regId){
+function insertRecord(registro){
     // const nuevaTarjeta = document.createElement()
+    if (cargaLS == false){
+    regId = Date.now()*(Math.round(Math.random()*100))
+    }
+    cargaLS = true
     const listaRegistros = document.getElementById('cardSection')
     listaRegistros.innerHTML +=`
     <div class="cards-container" id="Child${registro.regId}">
@@ -83,28 +121,31 @@ function insertRecord(registro, regId){
         <a id="${registro.regId}" onclick="deleteRecord(${registro.regId})" class="opt-btn eliminar" href="#">Eliminar</a>
     </div>
     </div>`
-    // console.log(registro.regId)
 }
 
 function saveDataLS(){
-    //console.log(tareas)
-    console.log(JSON.stringify(registros))
-    localStorage.setItem('registros',JSON.stringify(registros)) //setItem recibe dos parametros, 
+    localStorage.setItem('registros', JSON.stringify(registros)) //setItem recibe dos parametros, 
 }
 
 function readDataLS(){
-    const hist_registros = JSON.parse(localStorage.getItem('registros'))
-    hist_registros.foreach((el)=> createrow(el))
-    console.log(hist_registros)
+    hist_registros = JSON.parse(localStorage.getItem('registros'))
+    if (hist_registros !=null) {
+        hist_registros.forEach((el)=> registros.push(el))
+        hist_registros.forEach((el)=> insertRecord(el))
+    }else{
+        console.log('No hay datos para mostrar en LS')
+    }
 }
 
 function viewRecordDetails(value){
+    value = value
     const registro = document.getElementById("Child"+value) 
     const detalleRegistro = document.createElement('div')
+    let indice = registros.map(registro => registro.regId).indexOf(value)
     //acá estoy agregando el detalle de la actividad
     detalleRegistro.innerHTML =`
     <h2>Tarea asignada:<h2>
-    <h2>${registros[value].desc}<h2> 
+    <h2>${registros[indice].desc}<h2> 
     <a onclick="hideRecordDetails(${value})" class="opt-btn cerrar" href="#">Ocultar</a>
     `
     detalleRegistro.className='card-detail'
@@ -117,62 +158,63 @@ function hideRecordDetails(value){
     const registro = document.getElementById("Child"+value)
     const escondeRegistro = document.getElementById('Desc'+value)
     registro.removeChild(escondeRegistro)
-    console.log(escondeRegistro)
     registro.style.height = "40px";
 }
 
 function editRecord(value){
+    let indice = registros.map(registro => registro.regId).indexOf(value)
     editandoRegistro = true
-    editandoIndice = value
+    editandoIndice = indice
     const actualizaRegistro = document.getElementById('nuevoRegistro')
     actualizaRegistro.innerHTML = 'Actualizar Registro'
     // vuelve a cargar la información en el formulario 
-    document.getElementById('id').value = registros[value].id
-    document.getElementById('name').value = registros[value].name
-    document.getElementById('lastName').value = registros[value].lastname
-    document.getElementById('projectStatus').value = registros[value].projectStatus
-    document.getElementById('assignDate').value = registros[value].assignDate
-    document.getElementById('desc').value = registros[value].desc
+    regId = registros[indice].regId
+    console.log(regId)
+    document.getElementById('id').value = registros[indice].id
+    document.getElementById('name').value = registros[indice].name
+    document.getElementById('lastName').value = registros[indice].lastname
+    document.getElementById('projectStatus').value = registros[indice].projectStatus
+    document.getElementById('assignDate').value = registros[indice].assignDate
+    document.getElementById('desc').value = registros[indice].desc
     const modificando =document.getElementById(value)
     modificando.style.backgroundColor = "gray"
-    editandoIndice = value
-    console.log(editandoIndice)
+    // setTimeout(alertEditRecord(), 1000000)
 }
 
+// function alertEditRecord(value){
+//     console.log(value)
+//     const modificando =document.getElementById(value)
+//     modificando.style.backgroundColor = "#252525"
+// }
+
 function saveRecordupdate(value){
-    const registro = readform()
-    console.log(registro)
-    console.log(value)
-    const registroActualizado = registros.splice(value,1,registro)
-    saveDataLS()   
-    console.log(registroActualizado)
-    console.log(registros)
+    const registro = {
+        regId: registros[editandoIndice].regId,
+        id: document.getElementById('id').value,
+        name: document.getElementById('name').value,
+        lastname: document.getElementById('lastName').value,
+        projectStatus: document.getElementById('projectStatus').value,
+        assignDate: document.getElementById('assignDate').value,
+        desc: document.getElementById('desc').value,
+    }
+    registros.splice(editandoIndice,1,registro)
     const actualizaRegistro = document.getElementById('nuevoRegistro')
     actualizaRegistro.innerHTML = 'Guardar Registro'
     editandoRegistro = false
-    updCardDetails(registro,value)
-    const modificando =document.getElementById(value)
-    modificando.style.backgroundColor = "#252525"
+    document.getElementById('crdid'+regId).innerText = registros[editandoIndice].id
+    document.getElementById('crdName'+regId).innerText = registros[editandoIndice].name
+    document.getElementById('crdLastName'+regId).innerText = registros[editandoIndice].lastname
+    document.getElementById('crdProStatus'+regId).innerText = registros[editandoIndice].projectStatus
+    document.getElementById('crdAssDate'+regId).innerText = registros[editandoIndice].assignDate
     updProjectStatus()
     limpiarFormulario()
-}
-
-function updCardDetails(registro,value){
-    console.log(registro,value)
-    document.getElementById("crdid"+value).innerText = registro.id
-    document.getElementById("crdid"+value).innerText= registro.id
-    document.getElementById("crdName"+value).innerText = registro.name
-    document.getElementById("crdLastName"+value).innerText = registro.lastname
-    document.getElementById("crdProStatus"+value).innerText = registro.projectStatus
-    document.getElementById("crdAssDate"+value).innerText = registro.assignDate
-    // document.getElementById('crddesc').innerText = registro.desc
+    saveDataLS()
 }
 
 function deleteRecord(value){
-    const registroEliminado = registros.splice(value,1)
-    saveDataLS()   
-    console.log(registroEliminado)
-    console.log(registros)
+    let indice = registros.map(registro => registro.regId).indexOf(value)
+    registros.splice(indice,1)
+    saveDataLS()  
     const listaRegistros = document.getElementById('cardSection') 
     const registro = document.getElementById("Child"+value)
     listaRegistros.removeChild(registro)
@@ -181,6 +223,7 @@ function deleteRecord(value){
 }
 
 function updProjectStatus(){
+    if (registros != null){
     const pAssi = registros.filter(registros => registros.projectStatus === "Asignado")
     const pPdte = registros.filter(registros => registros.projectStatus === "Pendiente")
     const pComp = registros.filter(registros => registros.projectStatus === "Completado")
@@ -188,4 +231,5 @@ function updProjectStatus(){
     document.getElementById("nProAss").innerText = `${pAssi.length} Proyecto(s) asignados.`
     document.getElementById("nProCom").innerText = `${pComp.length} Proyecto(s) completados.`
     document.getElementById("nProPdt").innerText = `${pPdte.length} Proyecto(s) pendientes.`
+    }
 }
